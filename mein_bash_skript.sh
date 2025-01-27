b51cbcfa1c6acc112989ca8835b4d305206f77bb
Rabea Müller <muellerr@zbmed.de>
2025-01-27 14:53:35 +0100

    Initial commit

---
+++ mein_bash_skript.sh
@@ -0,0 +1,13 @@
+# Das ist mein Skript mit meiner For-Schleife
+
+echo "Skript beginnt."
+
+for FILE in *.txt
+do
+echo "$FILE ist eine tolle Datei"
+echo "--------"
+done
+
+echo "Skript beendet."
+
+
